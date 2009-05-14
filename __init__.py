7fe4521db07caeeb90ce73e30480fa499f244fd6
Taverne Sylvain <sylvain@itaapy.com>
2009-05-14 15:57:41 +0200

    i18n: Add "shop" domain.

--- __init__.py
+++ __init__.py
@@ -15,14 +15,22 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # Import from itools
-from itools import get_version
+from itools import get_version, get_abspath
 
-# Import from package
+# Import from ikaaro
+from itools.gettext import register_domain
+
+# Import from shop
 from shop import Shop
 
+# Import from package
 import skin
 import user
 import shipping_modes
 
 # Make the product version available to Python code
 __version__ = get_version()
+
+# Register the shop domain
+path = get_abspath('locale')
+register_domain('shop', path)
