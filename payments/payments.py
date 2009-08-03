f0ca20459657d47c4aa6c0520e1c86776faffa46
Taverne Sylvain <sylvain@itaapy.com>
2009-08-03 16:35:53 +0200

    Payments: In make_resource we init all payment ways.

--- payments/payments.py
+++ payments/payments.py
@@ -51,6 +51,14 @@ class Payments(ShopFolder):
         return payment_ways_registry.values()
 
 
+    @staticmethod
+    def _make_resource(cls, folder, name, *args, **kw):
+        # Init payment ways
+        for payment_way_name, payment_way_cls in payment_ways_registry.items():
+            payment_way_cls._make_resource(payment_way_cls, folder,
+                '%s/%s' % (name, payment_way_name))
+
+
     ######################
     # Public API
     ######################
