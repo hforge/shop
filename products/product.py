140f704b329ebe69ad3aee456e58a32d0e1e9b38
Nicolas Deram <nicolas@itaapy.com>
2010-01-29 18:28:38 +0100

    tags: Use viewbox as default tag_view.

--- products/product.py
+++ products/product.py
@@ -93,7 +93,7 @@ class Product(WorkflowAware, Editable, DynamicFolder):
     # Views
     #######################
     view = Product_View()
-    tag_view = None
+    tag_view = viewbox
     edit = Product_Edit()
     add_link_file = Product_AddLinkFile()
     change_product_model = Product_ChangeProductModel()
